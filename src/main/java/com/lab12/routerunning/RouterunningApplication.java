f4626688a4b8cf362cb01d9574531a16bd923cee
Darrin Howell <dbhowell09@gmail.com>
2018-12-11 09:37:36 -0800

    initial commit from Spring init

---
+++ src/main/java/com/lab12/routerunning/RouterunningApplication.java
@@ -0,0 +1,12 @@
+package com.lab12.routerunning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouterunningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RouterunningApplication.class, args);
+	}
+}
