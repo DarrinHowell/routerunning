da12f4b526174ebb0f7760d3f8a0f231087cefca
Darrin Howell <dbhowell09@gmail.com>
2018-12-11 12:16:37 -0800

    created /capitalize route

--- src/main/java/com/lab12/routerunning/HelloWorldController.java
+++ src/main/java/com/lab12/routerunning/HelloWorldController.java
@@ -1,13 +1,20 @@
 package com.lab12.routerunning;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+
     @RequestMapping("/hello")
     public String helloWorld(){
         return "Hello world!";
+    }
+
+    @RequestMapping("/capitalize/{contentToCapitalize}")
+    public String capitalizePath(@PathVariable () String contentToCapitalize){
+        return contentToCapitalize.toUpperCase();
     }
 
 }
