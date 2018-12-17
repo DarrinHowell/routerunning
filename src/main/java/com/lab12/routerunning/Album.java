99d8783455c66037fa06e8f9eafcf55a90e641b5
Darrin Howell <dbhowell09@gmail.com>
2018-12-16 16:20:17 -0800

    posting albums to database and rendering

--- src/main/java/com/lab12/routerunning/Album.java
+++ src/main/java/com/lab12/routerunning/Album.java
@@ -1,23 +1,34 @@
 package com.lab12.routerunning;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.nio.file.Path;
 
+@Entity
 public class Album {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
     private String title;
     private String artist;
     private int songCount;
     private int length;
-    private Path imageURL;
+//    private Path imageURL;
 
     public Album(String title, String artist, int songCount, int length) {
         this.title = title;
         this.artist = artist;
         this.songCount = songCount;
         this.length = length;
-        this.imageURL = null;
+//        this.imageURL = null;
     }
 
+    public Album() {}
+
     public String toString(){
-        return "Album title: " + this.title + " Album artist: " + this.artist + " Song count: " + this.songCount;
+        return "Album title: " + this.title + ". Album artist: " + this.artist + ". Song count: " + this.songCount + ".";
     }
 }
--- src/main/java/com/lab12/routerunning/AlbumController.java
+++ src/main/java/com/lab12/routerunning/AlbumController.java
@@ -1,22 +1,33 @@
 package com.lab12.routerunning;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class AlbumController {
+    @Autowired
+    public AlbumRepository albumRepo;
 
     @RequestMapping(value = "/albums", method = RequestMethod.GET)
     public String getAllAlbums(Model m){
 
-        Album[] albums = new Album[]{
-                new Album("The Arrow", "Andy Mineo", 6, 25),
-                new Album("All Things Work Together", "Lecrae", 12, 48)
-        };
-
-        m.addAttribute("albums", albums);
+//        Album[] albums = new Album[]{
+//                new Album("The Arrow", "Andy Mineo", 6, 25),
+//                new Album("All Things Work Together", "Lecrae", 12, 48)
+//        };
+//
+        m.addAttribute("albums", albumRepo.findAll());
         return "albums";
+    }
+
+    @RequestMapping(value = "/albums", method = RequestMethod.POST)
+    public RedirectView createAlbums(){
+        Album newAlbum = new Album("The Arrow", "Andy Mineo", 6, 25);
+        albumRepo.save(newAlbum);
+        return new RedirectView("/albums");
     }
 }
